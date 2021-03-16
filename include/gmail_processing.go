8016e19f32b2427afaab41b16baffc3b1372607b
art-k <artem.kryhin@gmail.com>
2021-03-16 20:46:36 +0200

    email fixes

--- include/gmail_processing.go
+++ include/gmail_processing.go
@@ -98,18 +98,8 @@ func SendEmailOAUTH2(to string, subj string, body string) (status string, messag
 		subject +
 		mime +
 		"<html>" +
-		"<style>" +
-		"table.center {" +
-		"min-width:200px;" +
-		"max-width:700px;" +
-		"width:80%" +
-		"text-align: justify;" +
-		"margin-left: auto;" +
-		"margin-right: auto;" +
-		"}" +
-		"</style>" +
 		"<div style='text-align: center;'><img style='width:60px;height:75px;' src='" + os.Getenv("DOMAIN") + "/logo/" + dbOutMessage.ID + "' alt=''></div><p>" +
-		"<table class='center'><tr><td>" +
+		"<table style='min-width:200px; max-width:700px; width:80%; text-align:justify; margin-left:auto; margin-right:auto;'><tr><td>" +
 		body +
 		"</td></tr><tr style='padding:15px'><td style='text-align: center;font-size: x-small;'>powered by <a href='https://www.maxcondoclub.com'>www.maxcondoclub.com</a></td></tr></table>"
 	dbOutMessage.OutMessage = msgString
