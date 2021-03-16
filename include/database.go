4cfa63867224132cab7cf6041f013e24637ba949
art-k <artem.kryhin@gmail.com>
2021-03-16 11:33:19 +0200

    some email modifications

--- include/database.go
+++ include/database.go
@@ -14,9 +14,9 @@ func initDatabase() {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold: time.Second, // Slow SQL threshold
-			LogLevel:      logger.Info, // Log level
-			Colorful:      true,        // Disable color
+			SlowThreshold: time.Second,  // Slow SQL threshold
+			LogLevel:      logger.Error, // Log level
+			Colorful:      true,         // Disable color
 		},
 	)
 
