11f6f82042cf9f844ceaaf7d3e6c48c7b48e7300
art-k <artem.kryhin@gmail.com>
2021-03-16 20:56:19 +0200

    email fixes

--- include/gmail_processing.go
+++ include/gmail_processing.go
@@ -98,10 +98,11 @@ func SendEmailOAUTH2(to string, subj string, body string) (status string, messag
 		subject +
 		mime +
 		"<html>" +
-		"<div style='text-align: center;'><img style='width:60px;height:75px;' src='" + os.Getenv("DOMAIN") + "/logo/" + dbOutMessage.ID + "' alt=''></div><p>" +
-		"<table style='min-width:200px; max-width:700px; width:80%; text-align:justify; margin-left:auto; margin-right:auto;'><tr><td>" +
+		"<table style='min-width:200px; max-width:700px; width:80%; text-align:justify; margin-left:auto; margin-right:auto;'>" +
+		"<tr><td style='background:azure;'><div style='text-align: center;'><img style='width:60px;height:75px;' src='" + os.Getenv("DOMAIN") + "/logo/" + dbOutMessage.ID + "' alt=''></div></td></tr>" +
+		"<tr><td>" +
 		body +
-		"</td></tr><tr style='padding:15px'><td style='text-align: center;font-size: x-small;'>powered by <a href='https://www.maxcondoclub.com'>www.maxcondoclub.com</a></td></tr></table>"
+		"</td></tr><tr style='background:azure;padding:15px'><td style='text-align: center;font-size: x-small;'>powered by <a href='https://www.maxcondoclub.com'>www.maxcondoclub.com</a></td></tr></table>"
 	dbOutMessage.OutMessage = msgString
 
 	msg := []byte(msgString)
